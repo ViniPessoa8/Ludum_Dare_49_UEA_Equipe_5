651bad37eb4cb80e02af6633f56f023b34fce0d3
Daniel Malcher <dbm.eng18@uea.edu.br>
2021-10-03 18:56:48 -0400

    Bugfix Créditos

--- Thebuggers/Assets/Scripts/Background_Fade.cs
+++ Thebuggers/Assets/Scripts/Background_Fade.cs
@@ -17,11 +17,11 @@ public class Background_Fade : MonoBehaviour
   
   IEnumerator FadeTo(float aValue, float aTime)
   {
-      float alpha = transform.GetComponent<Renderer>().material.color.a;
+      float alpha = transform.GetComponent<SpriteRenderer>().material.color.a;
       for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
       {
           Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha,aValue,t));
-          transform.GetComponent<Renderer>().material.color = newColor;
+          transform.GetComponent<SpriteRenderer>().material.color = newColor;
           yield return null;
       }
   }
